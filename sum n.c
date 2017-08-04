c44aef2f05f4d580c1553851d40a2f6d2263a0b4
gokulraj5 <30368242+gokulraj5@users.noreply.github.com>
2017-08-04 16:57:04 +0530

    Create sum n.c

---
+++ sum n.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+  int Num, i, Sum = 0;
+  scanf("%d", &Number);
+  
+  for(i = 1; i <= Num; i++)
+  {
+     Sum = Sum + i;
+  }
+  
+  printf("Sum of Natural Numbers = %d", Sum);
+  return 0;
+}
