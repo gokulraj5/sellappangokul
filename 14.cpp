d8b71b6913c83db9fa19f39e5f3b69fe9af7f880
gokulraj5 <30368242+gokulraj5@users.noreply.github.com>
2017-08-11 16:46:20 +0530

    Create 14.cpp

---
+++ 14.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ int b;
+ int i;
+ cout<<" enter number "<<endl;
+ cin>>b;
+ cout<<" enter number "<<endl;
+ cin>>i;
+ cout<<"*********************************"<<endl;
+ cout<<" programology student site "<<endl;
+ cout<<"**********************************"<<endl;
+ for(;b<=i;)
+ {
+ cout<<b<<endl;
+ b=b+2;
+ }
+}
