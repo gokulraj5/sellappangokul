b61fe66cd5fed2cfb37d47239147c9b887c221d6
gokulraj5 <30368242+gokulraj5@users.noreply.github.com>
2017-08-11 17:29:02 +0530

    Create 20.c

---
+++ 20.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int num, j;
+    printf("Enter an integer: ");
+    scanf("%d",&num);
+    for(j=1; j<=5; ++j)
+    {
+        printf("%d * %d = %d \n", num, j, n*j);
+    }
+    return 0;
+}
