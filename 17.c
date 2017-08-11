fc879bcf07a686f612d8675353a30f45a609f0a5
gokulraj5 <30368242+gokulraj5@users.noreply.github.com>
2017-08-11 17:19:48 +0530

    Create 17.c

---
+++ 17.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int lo, hi, j, t1, t2, remain, num = 0, result = 0;
+    printf("Enter two numbers(intervals): ");
+    scanf("%d %d", &lo, &hi);
+    printf("Armstrong numbers between %d an %d are: ", lo, hi);
+    for(j= lo + 1; j < high; ++j)
+    {
+        t2 = i;
+        t1 = i;
+        while (temp1 != 0)
+        {
+            t1 /= 10;
+            ++n;
+        }
+        while (t2 != 0)
+        {
+            remain = t2 % 10;
+            result += pow(remaind, num);
+            t2 /= 10;
+        }
+        if (result == i) {
+            printf("%d ", j);
+     }
+        num = 0;
+        result = 0;
+    }
+    return 0;
+}
